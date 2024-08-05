b8a606bd060b0221e2833c94ff988f267871ebce
jh0523 <ljhsky0523@naver.com>
2024-08-05 17:25:55 +0900

    upload

--- src/main/java/kr/or/iei/photo/model/dto/PhotoRowMapper.java
+++ src/main/java/kr/or/iei/photo/model/dto/PhotoRowMapper.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class PhotoRowMapper implements RowMapper<Photo> {
 	@Override
--- src/main/java/kr/or/iei/photo/model/service/PhotoService.java
+++ src/main/java/kr/or/iei/photo/model/service/PhotoService.java
@@ -2,11 +2,19 @@ package kr.or.iei.photo.model.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import kr.or.iei.photo.model.dao.PhotoDao;
+import kr.or.iei.photo.model.dto.Photo;
 
 @Service
 public class PhotoService {
 	@Autowired
 	private PhotoDao photoDao;
+	@Transactional
+	public int insertPhoto(Photo p) {
+		// TODO Auto-generated method stub
+		int result = photoDao.insertPhoto(p);
+		return result;
+	}
 }
