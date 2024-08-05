fbef47890b6dca759fd422bbe11bb66beebbbc93
jh0523 <ljhsky0523@naver.com>
2024-08-05 20:22:39 +0900

    createComment

---
+++ src/main/java/kr/or/iei/photo/model/dto/PhotoComment.java
@@ -0,0 +1,16 @@
+package kr.or.iei.photo.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PhotoComment {
+	private int photoFeedCommentNo;
+	private String photoFeedCommentWriter;
+	private String photoFeedCommentContent;
+	private String photoFeedCommentDate;
+	private int photoRef;
+}
