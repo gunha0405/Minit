33838f29adfe4ab0f50a2d6bb46356791b6fcd33
heesoo <goqkddnf@naver.com>
2024-08-06 20:56:19 +0900

    feed) feedWrite 구현

--- src/main/java/kr/or/iei/feed/model/dto/Feed.java
+++ src/main/java/kr/or/iei/feed/model/dto/Feed.java
@@ -16,6 +16,5 @@ public class Feed {
 	private String userFeedContnet;
 	private String userFeedDate;
 	private int userFeedCount;
-	private List<User> user;
-	private List<FeedFile> fileList;
+	private User user;
 }
