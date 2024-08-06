d7bf9b69459edfce22e24463ed8eff369add9246
heesoo <goqkddnf@naver.com>
2024-08-06 09:40:57 +0900

    보드컨트롤러 충돌 해결

--- src/main/java/kr/or/iei/board/controller/BoardController.java
+++ src/main/java/kr/or/iei/board/controller/BoardController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BoardController {
-<<<<<<< HEAD
-=======
+
+
 //@RequestMapping
->>>>>>> ba035b7ab04a35cbdff3fb9a87034351b0d2a372
+
 }
